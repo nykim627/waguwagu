f25054d15f81d16690ae2a0f40498acb6252d15e
leehyojin <lizzie0315@naver.com>
2025-04-03 16:30:50 +0900

    user 기능 구현 완료

--- WAGUWAGU/src/main/java/com/waguwagu/controller/UserController.java
+++ WAGUWAGU/src/main/java/com/waguwagu/controller/UserController.java
@@ -32,6 +32,8 @@ public class UserController extends HttpServlet {
     	System.out.println(act);
     	
     	switch(act) {
+    	case "checkId":
+    		doCheckId(request, response);
     	case "loginform":
     		doLoginForm(request, response);
     		break;
@@ -50,19 +52,53 @@ public class UserController extends HttpServlet {
     	}
     }
 
-	private void doRegist(HttpServletRequest request, HttpServletResponse response) {
+	private void doCheckId(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userId = request.getParameter("userId");
 		
+		//아이디 중복 체크 서비스 호출
+		if(service.checkId(userId)) {
+			request.setAttribute("errorMessage", "이미 존재하는 아이디입니다.");
+			request.getRequestDispatcher("/WEB-INF/user/registform.jsp").forward(request, response);
+		}else {
+			request.setAttribute("errorMessage", "해당 아이디로 회원가입이 가능합니다.");
+			request.getRequestDispatcher("/WEB-INF/user/registform.jsp").forward(request, response);
+		}
 	}
 
-	private void doRegistForm(HttpServletRequest request, HttpServletResponse response) {
+	private void doRegist(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String userId = request.getParameter("userId");
+		String userPassword = request.getParameter("userPassword");
+		String userEmail = request.getParameter("userEmail");
+		String userName = request.getParameter("userName");
+		String userGender = request.getParameter("userGender");
 		
+		//회원가입 서비스 호출 (유저 객체 반환)
+		User registedUser = new User(userId, userPassword, userName, userGender, userEmail);
+		boolean isRegistOK = service.joinUser(registedUser);
+		
+		System.out.println(registedUser.toString());
+		System.out.println(isRegistOK);
+		if(isRegistOK) {
+			//회원가입 성공 후 로그인 페이지로 이동
+			request.setAttribute("errorMessage", "회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.");
+			request.getRequestDispatcher("/WEB-INF/user/loginform.jsp").forward(request, response);  // 서버 내부 이동을 위해 포워딩
+		}else {
+			//회원가입 실패 -> 회원가입 페이지로 돌아가기
+			request.setAttribute("errorMessage", "다시 시도해주세요.");
+			request.getRequestDispatcher("/WEB-INF/user/registform.jsp").forward(request, response);
+		}
 	}
 
-	private void doLogout(HttpServletRequest request, HttpServletResponse response) {
+	private void doRegistForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/user/registform.jsp").forward(request, response);
+	}
+
+	private void doLogout(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		HttpSession session = request.getSession();
 		session.removeAttribute("loggedInUser"); //세션에서 유저 정보 제거
 		
-//		response.sendRedirect("index.jsp");
+		//로그아웃 후 메인페이지로 이동
+		response.sendRedirect("index.jsp");
 	}
 
 	private void doLogin(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
--- WAGUWAGU/src/main/java/com/waguwagu/service/UserService.java
+++ WAGUWAGU/src/main/java/com/waguwagu/service/UserService.java
@@ -7,6 +7,9 @@ public interface UserService {
     //로그인
 	User loginUser(String userId, String password);
 	
+	//아이디 중복 확인
+	boolean checkId(String userId);
+	
 	//회원가입
 	boolean joinUser(User user);
 
