d3b83b3499b18403e2e0a5f1753117227ea34666
nayoung <hkny0627@gmail.com>
2025-04-08 16:21:06 +0900

    [back] homecontroller 수정

--- WAGUWAGU/src/main/java/com/waguwagu/controller/HomeController.java
+++ WAGUWAGU/src/main/java/com/waguwagu/controller/HomeController.java
@@ -7,13 +7,13 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/")
+@WebServlet("/home")
 public class HomeController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		System.out.println("MainPage");
-		response.sendRedirect("video?act=viewCnt");
+		response.sendRedirect("index2.jsp");
 	}
 }
